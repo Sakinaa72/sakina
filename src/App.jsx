bba953f6e7e533be55df5a1e6c6f8d1787a50b6a
Yasin Bhojani <yasinbhojani333@gmail.com>
2023-10-15 20:21:35 +0530

    portfolio commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className="text-center font-medium mt-10 text-blue-600">
+      <h1 className={`text-4xl font-bold`}>Yasin B.</h1>
+      <p className="my-5">A Fullstack Developer, Tech Enthusiast and Investor.</p>
+    </div>
+  );
+}
+
+export default App;
