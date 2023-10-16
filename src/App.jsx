e9c2f5fb55358c6b319e514b28eb04e2f4d92886
Yasin Bhojani <74419103+yasinbhojani@users.noreply.github.com>
2023-10-16 15:39:03 +0000

    added links

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ function App() {
     <div className="text-center font-medium mt-10 text-blue-600">
       <h1 className={`text-4xl font-bold`}>Yasin Bhojani</h1>
       <p className="my-5">A Fullstack Developer, Tech Enthusiast and Investor.</p>
+      <ul className="flex items-center justify-center gap-3 underline">
+        <li><a href="https://github.com/yasinbhojani" target="_blank">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/yasinbhojani/" target="_blank">Linkedin</a></li>
+        <li><a href="https://twitter.com/yasin_bhojani" target="_blank">X</a></li>
+      </ul>
     </div>
   );
 }
