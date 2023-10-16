df87dfbee4835912661f500405d8877b0c11aa29
Yasin Bhojani <yasinbhojani333@gmail.com>
2023-10-16 23:06:02 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="text-center font-medium mt-10 text-blue-600">
+    <div className="text-center font-medium mt-10 text-blue-600 mx-3">
       <h1 className={`text-4xl font-bold`}>Yasin Bhojani</h1>
       <p className="my-5">A Fullstack Developer, Tech Enthusiast and Investor.</p>
     </div>
